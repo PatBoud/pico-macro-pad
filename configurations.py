f2f1409927f5d3101409eb3dad6d001e811a21c5
PatBoud <patboud@MacBook-Pro-de-PatBoud.local>
2021-05-19 19:54:55 -0400

    Nouvelles configurations

--- configurations.py
+++ configurations.py
@@ -13,85 +13,6 @@ from adafruit_hid.consumer_control_code import ConsumerControlCode
 keyboard = Keyboard(usb_hid.devices)
 layout = KeyboardLayoutUS(keyboard)
 
-## CONFIGURATIONS ##
-#===========================================#
-# class Terminal(AbstractConfiguration):
-# 	def getName():
-# 		return 'Terminal'
-# 	def getColor():
-# 		return (0, 255, 0)
-# 	def getMacros():
-# 		return [
-# 			Ls, 
-# 			Pwd,
-# 			Home
-# 		]
-
-# class Ls(AbstractMacro):
-# 	def getMacroName():
-# 		return 'ls -al'
-# 	def getMacro():
-# 		layout.write("ls ")
-# 		keyboard.send(Keycode.KEYPAD_MINUS)
-# 		layout.write("al")
-# 		keyboard.send(Keycode.ENTER)
-
-# class Pwd(AbstractMacro):
-# 	def getMacroName():
-# 		return 'pwd'
-# 	def getMacro():
-# 		layout.write("pwd")
-# 		keyboard.send(Keycode.ENTER)
-
-# class Home(AbstractMacro):
-# 	def getMacroName():
-# 		return 'Home'
-# 	def getMacro():
-# 		layout.write("cd ")
-# 		keyboard.send(Keycode.ENTER)
-#===========================================#
-
-
-# class Git(AbstractConfiguration):
-# 	def getName():
-# 		return 'GIT'
-# 	def getColor():
-# 		return (247, 78, 39)
-# 	def getMacros():
-# 		return [
-# 			MergeDevelop,
-# 			MergeMaster,
-# 			GitPush
-# 		]
-
-# class MergeDevelop(AbstractMacro):
-# 	def getMacroName():
-# 		return "Merge develop into master"
-# 	def getMacro():
-# 		layout.write("git checkout master")
-# 		keyboard.send(Keycode.ENTER)
-# 		time.sleep(0.5)
-# 		layout.write("git merge develop")
-# 		keyboard.send(Keycode.ENTER)
-
-# class MergeMaster(AbstractMacro):
-# 	def getMacroName():
-# 		return "Merge master into develop"
-# 	def getMacro():
-# 		layout.write("git checkout develop")
-# 		keyboard.send(Keycode.ENTER)
-# 		time.sleep(0.5)
-# 		layout.write("git merge master")
-# 		keyboard.send(Keycode.ENTER)
-
-# class GitPush(AbstractMacro):
-# 	def getMacroName():
-# 		return "Push"
-# 	def getMacro():
-# 		layout.write("git push")
-# 		keyboard.send(Keycode.ENTER)
-#===========================================#
-
 group1 = (64,8,8) #red
 group2 = (64,32,8) #orange
 group3 = (64,64,8) #yellow
@@ -100,6 +21,9 @@ group5 = (8,8,64) #blue
 group6 = (30,8,64) #indigo
 group7 = (32,8,64) #violet
 group8 = (8,64,64) #teal
+
+color_git = (247,78,39)
+
 
 consumer_control = ConsumerControl(usb_hid.devices)
 
@@ -290,29 +214,178 @@ class med_volDown(AbstractMacro):
         return (group7[0]*.25,group7[1]* .75, group7[2]* 1.25) 
 
 
-#consumer_control.press(ConsumerControlCode.VOLUME_INCREMENT)
-#        time.sleep(0.5)
-#        consumer_control.release()
+#===========================================#
 
-## COMMANDS ##
+
+class Terminal(AbstractConfiguration):
+	def getName():
+		return 'Terminal'
+	def getColor():
+		return group4
+	def getMacros():
+		return [
+			Ls, 
+			Pwd,
+			Home
+		]
+
+class Ls(AbstractMacro):
+	def getMacroName():
+		return 'ls -al'
+	def getMacro():
+		layout.write("ls ")
+		keyboard.send(Keycode.KEYPAD_MINUS)
+		layout.write("al")
+		keyboard.send(Keycode.ENTER)
+	def getMacroColor():
+		return (group4[0]*.25,group4[1]* .75, group4[2]* 1.25) 
+
+class Pwd(AbstractMacro):
+	def getMacroName():
+		return 'pwd'
+	def getMacro():
+		layout.write("pwd")
+		keyboard.send(Keycode.ENTER)
+	def getMacroColor():
+		return (group4[0]*.25,group4[1]* .75, group4[2]* 1.25) 
+
+class Home(AbstractMacro):
+	def getMacroName():
+		return 'Home'
+	def getMacro():
+		layout.write("cd ")
+		keyboard.send(Keycode.ENTER)
+	def getMacroColor():
+		return (group4[0]*.25,group4[1]* .75, group4[2]* 1.25) 
+
+
+#===========================================#
+
+class OBS(AbstractConfiguration):
+	def getName():
+		return 'OBS'
+	def getColor():
+		return group5
+	def getMacros():
+		return [
+			SelectScene1, 
+			SelectScene2,
+			SelectScene3,
+			MuteOn,
+			MuteOff
+		]
+
+class SelectScene1(AbstractMacro):
+	def getMacroName():
+		return 'Scene 1'
+	def getMacro():
+		keyboard.send(Keycode.LEFT_ALT, Keycode.ONE)
+	def getMacroColor():
+		return (group5[0]*.25,group5[1]* .75, group5[2]* 1.25) 
+
+class SelectScene2(AbstractMacro):
+	def getMacroName():
+		return 'Scene 2'
+	def getMacro():
+		keyboard.send(Keycode.LEFT_ALT, Keycode.TWO)
+	def getMacroColor():
+		return (group5[0]*.25,group5[1]* .75, group5[2]* 1.25) 
+
+class SelectScene3(AbstractMacro):
+	def getMacroName():
+		return 'Scene 3'
+	def getMacro():
+		keyboard.send(Keycode.LEFT_ALT, Keycode.THREE)
+	def getMacroColor():
+		return (group5[0]*.25,group5[1]* .75, group5[2]* 1.25) 
+
+class MuteOn(AbstractMacro):
+	def getMacroName():
+		return 'Mute'
+	def getMacro():
+		keyboard.send(Keycode.LEFT_ALT, Keycode.FOUR)
+	def getMacroColor():
+		return (group5[0]*.25,group5[1]* .75, group5[2]* 1.25) 
+
+class MuteOff(AbstractMacro):
+	def getMacroName():
+		return 'Unmute'
+	def getMacro():
+		keyboard.send(Keycode.LEFT_ALT, Keycode.FIVE)
+	def getMacroColor():
+		return (group5[0]*.25,group5[1]* .75, group5[2]* 1.25) 
+
+
+#===========================================#
+
+class Git(AbstractConfiguration):
+	def getName():
+		return 'GIT'
+	def getColor():
+		return color_git
+	def getMacros():
+		return [
+			GitStatus,
+			GitDiff
+			#MergeDevelop,
+			#MergeMaster,
+			#GitPush
+		]
+
+class GitStatus(AbstractMacro):
+	def getMacroName():
+		return "Git status"
+	def getMacro():
+		layout.write("git status")
+		keyboard.send(Keycode.ENTER)
+	def getMacroColor():
+		return (color_git[0]*.75,color_git[1]* .75, color_git[2]* 1.25) 
+
+class GitDiff(AbstractMacro):
+	def getMacroName():
+		return "Git diff"
+	def getMacro():
+		layout.write("git diff")
+		keyboard.send(Keycode.ENTER)
+	def getMacroColor():
+		return (color_git[0]*.25,color_git[1]* .75, color_git[2]* 1.25) 
+
+class MergeDevelop(AbstractMacro):
+	def getMacroName():
+		return "Merge develop into master"
+	def getMacro():
+		layout.write("git checkout master")
+		keyboard.send(Keycode.ENTER)
+		time.sleep(0.5)
+		layout.write("git merge develop")
+		keyboard.send(Keycode.ENTER)
+
+class MergeMaster(AbstractMacro):
+	def getMacroName():
+		return "Merge master into develop"
+	def getMacro():
+		layout.write("git checkout develop")
+		keyboard.send(Keycode.ENTER)
+		time.sleep(0.5)
+		layout.write("git merge master")
+		keyboard.send(Keycode.ENTER)
+
+class GitPush(AbstractMacro):
+	def getMacroName():
+		return "Push"
+	def getMacro():
+		layout.write("git push")
+		keyboard.send(Keycode.ENTER)
+
+#===========================================#
+
 
 # Map your configurations inside this array
-configurations_map = [Navigation, Media] #[Git, Navigation, Terminal] #, GoogleMeet, Obsidian, RandomEstimation, PhpStorm ]	
+configurations_map = [Navigation, Terminal, Media, OBS, Git] #[Git, Navigation, Terminal] #, GoogleMeet, Obsidian, RandomEstimation, PhpStorm ]	
 
 #=======================================================================
 #disabled configurations
 #=======================================================================
-
-# class GoogleMeet(AbstractConfiguration):
-# 	def getName():
-# 		return 'Google Meet'
-# 	def getColor():
-# 		return (255, 128, 0)
-# 	def getMacros():
-# 		return [
-# 			ToggleMicrophone, 
-# 			ToggleWebcam
-# 		]
 
 # class OBS(AbstractConfiguration):
 # 	def getName():
@@ -327,132 +400,6 @@ configurations_map = [Navigation, Media] #[Git, Navigation, Terminal] #, GoogleM
 # 			MuteOn,
 # 			MuteOff
 # 		]
-
-# class Obsidian(AbstractConfiguration):
-# 	def getName():
-# 		return 'Obsidian'
-# 	def getColor():
-# 		return (51, 51, 255)
-# 	def getMacros():
-# 		return [
-# 			AddNewLog
-# 		]
-
-# class RandomEstimation(AbstractConfiguration):
-# 	def getName():
-# 		return 'Random Esteem'
-# 	def getColor():
-# 		return (255, 0, 0)
-# 	def getMacros():
-# 		return [
-# 			OneWeekEsteem, 
-# 			OneMonthEsteem,
-# 			SixMonthsEsteem
-# 		]
-
-# class PhpStorm(AbstractConfiguration):
-# 	def getName():
-# 		return 'PHPStorm'
-# 	def getColor():
-# 		return (232, 49, 123)
-# 	def getMacros():
-# 		return [
-# 			OpenInTerminal,
-# 			Rename,
-# 			ComposerAutoload,
-# 			ListenForDebug,
-# 			StepOver,
-# 			StepInto,
-# 			StepOut,
-# 			NextBreakpoint
-# 		]
-
-#=======================================================================
-#disabled macros
-#=======================================================================
-
-# class OpenInTerminal(AbstractMacro):
-# 	def getMacroName():
-# 		return 'Open in terminal'
-# 	def getMacro():
-# 		keyboard.send(Keycode.ALT, Keycode.T)
-
-# class Rename(AbstractMacro):
-# 	def getMacroName():
-# 		return 'Rename'
-# 	def getMacro():
-# 		keyboard.send(Keycode.SHIFT, Keycode.F6)
-
-# class ListenForDebug(AbstractMacro):
-# 	def getMacroName():
-# 		return 'Listen for debug'
-# 	def getMacro():
-# 		keyboard.send(Keycode.ALT, Keycode.D)
-
-# class StepOver(AbstractMacro):
-# 	def getMacroName():
-# 		return 'Step over'
-# 	def getMacro():
-# 		keyboard.send(Keycode.F8)
-
-# class StepInto(AbstractMacro):
-# 	def getMacroName():
-# 		return 'Step into'
-# 	def getMacro():
-# 		keyboard.send(Keycode.F7)
-
-# class StepOut(AbstractMacro):
-# 	def getMacroName():
-# 		return 'Step out'
-# 	def getMacro():
-# 		keyboard.send(Keycode.SHIFT, Keycode.F8)
-
-# class NextBreakpoint(AbstractMacro):
-# 	def getMacroName():
-# 		return 'Next breakpoint'
-# 	def getMacro():
-# 		keyboard.send(Keycode.ALT, Keycode.COMMAND, Keycode.R)
-
-# class ComposerAutoload(AbstractMacro):
-# 	def getMacroName():
-# 		return 'Composer autoload'
-# 	def getMacro():
-# 		layout.write('composer dump')
-# 		keyboard.send(Keycode.KEYPAD_MINUS)
-# 		layout.write('autoload')
-# 		keyboard.send(Keycode.ENTER)
-
-# class OneWeekEsteem(AbstractMacro):
-# 	def getMacroName():
-# 		return '< 1 week'
-# 	def getMacro():
-# 		layout.write(str(random.randint(1, 5)))
-# 		keyboard.send(Keycode.ENTER)
-
-# class OneMonthEsteem(AbstractMacro):
-# 	def getMacroName():
-# 		return '< 1 month'
-# 	def getMacro():
-# 		layout.write(str(random.randint(5, 20)))
-# 		keyboard.send(Keycode.ENTER)
-
-# class SixMonthsEsteem(AbstractMacro):
-# 	def getMacroName():
-# 		return '< 6 months'
-# 	def getMacro():
-# 		layout.write(str(random.randint(20, 120)))
-# 		keyboard.send(Keycode.ENTER)
-# class ToggleMicrophone(AbstractMacro):
-# 	def getMacroName():
-# 		return 'Toggle Microphone'
-# 	def getMacro():
-# 		keyboard.send(Keycode.COMMAND, Keycode.D)
-
-# class ToggleWebcam(AbstractMacro):
-# 	def getMacroName():
-# 		return 'Toggle Webcam'
-# 	def getMacro():
-# 		keyboard.send(Keycode.COMMAND, Keycode.E)
 
 # class SelectScene1(AbstractMacro):
 # 	def getMacroName():
